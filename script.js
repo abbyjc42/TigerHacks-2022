56bfc7d08012ad6fd0b8bb9d48e08786316978a6
gabriellamiesner <ella.miesner@gmail.com>
2022-11-05 19:32:21 -0500

    built out basics

---
+++ script.js
@@ -0,0 +1,22 @@
+function data_priv() {
+  document.getElementById('card').style.visibility = "hidden";
+  document.getElementById('data').style.visibility = "visible";
+}
+
+function tap() {
+  document.getElementById('data').style.visibility = "hidden";
+  document.getElementById('tap').style.visibility = "visible";
+  
+}
+
+function omny() {
+    document.getElementById('data').style.visibility = "hidden";
+  document.getElementById('omny').style.visibility = "visible";
+  
+}
+
+function microchip() {
+    document.getElementById('data').style.visibility = "hidden";
+  document.getElementById('microchip').style.visibility = "visible";
+  
+}
