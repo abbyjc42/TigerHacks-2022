7f077f390a97538d86393e2598fc2875ffc18f91
Abby C <54291691+abbyjc42@users.noreply.github.com>
2022-11-05 21:36:00 -0500

    Added Click to Close Divs
    Now when any of the 3 opened infocards (Tap, Omny, or Microchip) are clicked, the card closes so the main view is visible again.

--- script.js
+++ script.js
@@ -10,13 +10,20 @@ function tap() {
 }
 
 function omny() {
-    document.getElementById('data').style.visibility = "hidden";
+  document.getElementById('data').style.visibility = "hidden";
   document.getElementById('omny').style.visibility = "visible";
   
 }
 
 function microchip() {
-    document.getElementById('data').style.visibility = "hidden";
+  document.getElementById('data').style.visibility = "hidden";
   document.getElementById('microchip').style.visibility = "visible";
   
+}
+
+function closediv() {
+  document.getElementById('data').style.visibility = "visible";
+  document.getElementById('tap').style.visibility = "hidden";
+  document.getElementById('omny').style.visibility = "hidden";
+  document.getElementById('microchip').style.visibility = "hidden";
 }
